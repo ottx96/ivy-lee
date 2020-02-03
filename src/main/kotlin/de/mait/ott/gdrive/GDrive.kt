1330aa088f4edb913397c860beb9e3a7a66a69ce
Manuel Ott <manuel.ott@computerkomplett.de>
2020-02-03 22:23:10 +0100

    tokens ignorieren

--- src/main/kotlin/de/mait/ott/gdrive/GDrive.kt
+++ src/main/kotlin/de/mait/ott/gdrive/GDrive.kt
@@ -12,7 +12,6 @@ import com.google.api.client.json.jackson2.JacksonFactory
 import com.google.api.client.util.store.FileDataStoreFactory
 import com.google.api.services.drive.Drive
 import com.google.api.services.drive.DriveScopes
-import com.google.api.services.drive.model.FileList
 import java.io.File
 import java.io.IOException
 import java.io.InputStreamReader
