9330e4a315da2780bd7a054ce1141a0505aa0a5f
Manuel Ott <manuel.ott@x.de>
2020-09-02 23:33:24 +0200

    Refactoring

--- src/main/kotlin/de/ott/ivy/gdrive/ConnectionProvider.kt
+++ src/main/kotlin/de/ott/ivy/gdrive/ConnectionProvider.kt
@@ -6,15 +6,12 @@ import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver
 import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow
 import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport
-import com.google.api.client.http.FileContent
 import com.google.api.client.http.javanet.NetHttpTransport
 import com.google.api.client.json.JsonFactory
 import com.google.api.client.json.jackson2.JacksonFactory
 import com.google.api.client.util.store.FileDataStoreFactory
 import com.google.api.services.drive.Drive
 import com.google.api.services.drive.DriveScopes
-import com.google.api.services.drive.model.File
-import com.google.api.services.drive.model.FileList
 import java.io.FileNotFoundException
 import java.io.IOException
 import java.io.InputStreamReader
@@ -35,19 +32,19 @@ object ConnectionProvider {
 
     /**
      * Creates an authorized Credential object.
-     * @param HTTP_TRANSPORT The network HTTP Transport.
+     * @param httpTransport The network HTTP Transport.
      * @return An authorized Credential object.
      * @throws IOException If the credentials.json file cannot be found.
      */
     @Throws(IOException::class)
-    private fun getCredentials(HTTP_TRANSPORT: NetHttpTransport): Credential {
+    private fun getCredentials(httpTransport: NetHttpTransport): Credential {
         // Load client secrets.
-        val `in` = ConnectionProvider::class.java.getResourceAsStream(CREDENTIALS_FILE_PATH)
-            ?: throw FileNotFoundException("Resource not found: " + CREDENTIALS_FILE_PATH)
-        val clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, InputStreamReader(`in`))
+        val inputRes = ConnectionProvider::class.java.getResourceAsStream(CREDENTIALS_FILE_PATH)
+            ?: throw FileNotFoundException("Resource not found: $CREDENTIALS_FILE_PATH")
+        val clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, InputStreamReader(inputRes))
 
         // Build flow and trigger user authorization request.
-        val flow = GoogleAuthorizationCodeFlow.Builder(HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
+        val flow = GoogleAuthorizationCodeFlow.Builder(httpTransport, JSON_FACTORY, clientSecrets, SCOPES)
             .setDataStoreFactory(FileDataStoreFactory(java.io.File(TOKENS_DIRECTORY_PATH)))
             .setAccessType("offline")
             .build()
@@ -58,8 +55,8 @@ object ConnectionProvider {
     @Throws(IOException::class, GeneralSecurityException::class)
     fun connect(): Drive {
         // Build a new authorized API client service.
-        val HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport()
-        return Drive.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
+        val httpTransport = GoogleNetHttpTransport.newTrustedTransport()
+        return Drive.Builder(httpTransport, JSON_FACTORY, getCredentials(httpTransport))
             .setApplicationName(APPLICATION_NAME)
             .build()
     }
