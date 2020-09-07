4112be04d7d722c9a2460a029fb00ffd87065855
Ott <49874532+ottx96@users.noreply.github.com>
2020-09-07 22:13:22 +0200

    Added Annotation (define display String on Button) (#44)
    Co-authored-by: Manuel Ott <manuel.ott@x.de>

--- example-extension/src/main/kotlin/de/ott/ivy/example/ExampleExtension.kt
+++ example-extension/src/main/kotlin/de/ott/ivy/example/ExampleExtension.kt
@@ -1,12 +1,14 @@
 package de.ott.ivy.example
 
 import de.ott.ivy.TaskExtension
+import de.ott.ivy.annotation.Extension
 import de.ott.ivy.data.IvyLeeTask
 
+@Extension("Example Extension")
 class ExampleExtension: TaskExtension {
 
     override fun execute(task: IvyLeeTask) {
-        task.descr = "Hello from ExampleExtension!"
+        task.descr = "Hello from ${javaClass.simpleName}!"
     }
 
 }
