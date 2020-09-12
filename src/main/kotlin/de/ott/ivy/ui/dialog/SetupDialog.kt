17edefaa6f735dfb6177bbe7c307c325dcb1b066
Manuel Ott <manuel.ott@x.de>
2020-09-12 11:47:02 +0200

    implementing pageFactory (Pagination)

--- src/main/kotlin/de/ott/ivy/ui/dialog/SetupDialog.kt
+++ src/main/kotlin/de/ott/ivy/ui/dialog/SetupDialog.kt
@@ -4,14 +4,21 @@ import de.ott.ivy.data.IvyLeeTask
 import javafx.scene.Parent
 import javafx.scene.Scene
 import javafx.scene.control.DialogPane
+import javafx.scene.control.Pagination
+import javafx.scene.layout.AnchorPane
 import javafx.scene.layout.BorderPane
 import javafx.scene.paint.Color
 import javafx.stage.Stage
 import tornadofx.View
+import tornadofx.borderpane
+import tornadofx.center
+import tornadofx.label
 
 class SetupDialog : View("Setup") {
 
-    override val root: BorderPane by fxml("/views/SetupDialog.fxml")
+    override val root: AnchorPane by fxml("/views/SetupDialog.fxml")
+
+    val pagination: Pagination by fxid("pagination")
 
     companion object {
         fun showDialog(){
@@ -19,6 +26,17 @@ class SetupDialog : View("Setup") {
                 scene = Scene(SetupDialog().root)
             }.showAndWait()
         }
+    }
+
+    init {
+        pagination.setPageFactory {
+            borderpane {
+                center {
+                    label("this is page $it") {  }
+                }
+            }
+        }
+        pagination.maxPageIndicatorCount = 50
     }
 
 }
