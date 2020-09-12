8ce614da3fd73a5fc8368ce809bdfa2439162472
Manuel Ott <manuel.ott@x.de>
2020-09-12 13:06:24 +0200

    implement language choosing

--- src/main/kotlin/de/ott/ivy/ui/dialog/SetupDialog.kt
+++ src/main/kotlin/de/ott/ivy/ui/dialog/SetupDialog.kt
@@ -1,15 +1,23 @@
 package de.ott.ivy.ui.dialog
 
+import de.ott.ivy.Entrypoint
+import javafx.beans.property.StringProperty
 import javafx.scene.Scene
 import javafx.scene.control.Pagination
+import javafx.scene.image.Image
 import javafx.scene.layout.AnchorPane
 import javafx.scene.layout.BorderPane
 import javafx.stage.Stage
 import tornadofx.View
+import tornadofx.doubleBinding
+import tornadofx.imageview
 
 class SetupDialog : View("Setup") {
 
     override val root: BorderPane by fxml("/views/SetupDialog.fxml")
+
+    val borderPaneGDrive: BorderPane by fxid("bp_gdrive")
+    val borderPaneLanguage: BorderPane by fxid("bp_language")
 
     companion object {
         fun showDialog(){
@@ -20,7 +28,8 @@ class SetupDialog : View("Setup") {
     }
 
     init {
-
+        title = "Ivy-Lee Task Tracker - Setup"
+        icon = imageview(Image(Entrypoint::class.java.getResourceAsStream("/de/ott/ivy/images/frog-hq.png")))
     }
 
 }
