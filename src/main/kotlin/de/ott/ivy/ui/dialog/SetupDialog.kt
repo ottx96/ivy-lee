5876edca106d6fe0270a93befc1063a1d2366a67
Manuel Ott <manuel.ott@x.de>
2020-09-12 13:26:20 +0200

    implement language choosing

--- src/main/kotlin/de/ott/ivy/ui/dialog/SetupDialog.kt
+++ src/main/kotlin/de/ott/ivy/ui/dialog/SetupDialog.kt
@@ -1,16 +1,16 @@
 package de.ott.ivy.ui.dialog
 
 import de.ott.ivy.Entrypoint
-import javafx.beans.property.StringProperty
+import javafx.event.EventHandler
 import javafx.scene.Scene
-import javafx.scene.control.Pagination
+import javafx.scene.control.ComboBox
+import javafx.scene.control.Label
 import javafx.scene.image.Image
-import javafx.scene.layout.AnchorPane
 import javafx.scene.layout.BorderPane
 import javafx.stage.Stage
 import tornadofx.View
-import tornadofx.doubleBinding
 import tornadofx.imageview
+import java.util.*
 
 class SetupDialog : View("Setup") {
 
@@ -18,6 +18,9 @@ class SetupDialog : View("Setup") {
 
     val borderPaneGDrive: BorderPane by fxid("bp_gdrive")
     val borderPaneLanguage: BorderPane by fxid("bp_language")
+
+    val languages: ComboBox<String> by fxid("languages")
+    val language: Label by fxid("language")
 
     companion object {
         fun showDialog(){
@@ -30,6 +33,17 @@ class SetupDialog : View("Setup") {
     init {
         title = "Ivy-Lee Task Tracker - Setup"
         icon = imageview(Image(Entrypoint::class.java.getResourceAsStream("/de/ott/ivy/images/frog-hq.png")))
+
+        Locale.getAvailableLocales().sortedBy { it.displayLanguage }.forEach {
+            if(!languages.items.contains(it.displayLanguage))
+                languages.items.add( it.displayLanguage )
+        }
+        languages.value = Locale.getDefault().displayLanguage
+        language.text = Locale.getDefault().language
+
+        languages.onAction = EventHandler {
+            language.text =  Locale.getAvailableLocales().firstOrNull {  it.displayLanguage == languages.value  }?.language?:"N/A"
+        }
     }
 
 }
