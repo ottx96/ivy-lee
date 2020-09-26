ef4e928c7879a1ca60d70ccf3a47ec4b82e7bfb8
Manuel Ott <manuel.ott@x.de>
2020-09-26 20:44:53 +0200

    Refactoring

--- github-extension/src/main/kotlin/de/ott/ivy/ext/github/html/MarkdownParser.kt
+++ github-extension/src/main/kotlin/de/ott/ivy/ext/github/html/MarkdownParser.kt
@@ -9,12 +9,7 @@ object MarkdownParser {
     private val parser: Parser by lazy { Parser.builder().extensions( listOf(AutolinkExtension.create())).build() }
     private val renderer: HtmlRenderer by lazy { HtmlRenderer.builder().extensions( listOf(AutolinkExtension.create())).build() }
     fun convertHtml(markdown: String): String{
-        var htmlString = renderer.render(parser.parse( markdown ))
-
-        if(htmlString.contains("""type="checkbox""""))
-            htmlString = htmlString.replace("""<li>""", "").replace("</li>", "<br>").replace("<ul>", "").replace("</ul>", "")
-
-        return htmlString
+        return renderer.render(parser.parse( markdown ))
     }
 
 }
