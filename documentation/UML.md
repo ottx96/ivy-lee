1f6a1f5897ae96c10cd21aff075924072d0aba46
Ott <49874532+ottx96@users.noreply.github.com>
2020-09-27 15:49:24 +0200

    created activity diagram (#82)
    * created activity diagram
    
    * extended UML.md
    
    * changed use case diagram
    
    Co-authored-by: Manuel Ott <manuel.ott@x.de>

--- documentation/UML.md
+++ documentation/UML.md
@@ -3,4 +3,8 @@ _**Get a better understanding of the application with these diagrams!**_
 
 ## Use-Case Diagram    
 ![use case diagram](files/uml-use-case.svg)  
-###### *(Generated from [src/main/kotlin/de/ott/ivy/Entrypoint.kt](../src/main/kotlin/de/ott/ivy/Entrypoint.kt))*  
+###### *(Generated from [src/main/kotlin/de/ott/ivy/Entrypoint.kt](../src/main/kotlin/de/ott/ivy/Entrypoint.kt))*
+
+## Activity Diagram
+![acitivty diagram](files/uml-activity-diagram.svg)  
+###### *(Generated from [src/main/kotlin/de/ott/ivy/Entrypoint.kt](../src/main/kotlin/de/ott/ivy/Entrypoint.kt))*
--- src/main/kotlin/de/ott/ivy/Entrypoint.kt
+++ src/main/kotlin/de/ott/ivy/Entrypoint.kt
@@ -22,22 +22,59 @@ import kotlin.system.exitProcess
  * Runs [IvyLee].
  * @see [IvyLee]
  *
+ * UML: use case diagram
  * @startuml
  * frame "uc main application" {
  * left to right direction
- * actor user
  * actor gdrive
+ * actor user
  *  rectangle application {
+ *      gdrive -- (setup)
  *      (edit task) .> (create task): <<include>>
  *      gdrive --> (synchronize)
  *      (synchronize) ..> (create task): <<extend>>
  *      (synchronize) ..> (edit task): <<extend>>
  *      user -- (setup)
- *      gdrive -- (setup)
  *      user --> (create task)
  *      user --> (edit task)
  *  }
  * }
+ * @enduml
+ *
+ * UML: activity diagram
+ * @startuml
+ * |User|
+ * start
+ * :right click task cell;
+ * |Application|
+ * if (task present?) then (true)
+ *   :load task instance (task data);
+ * else (no)
+ *   :initialize empty task instance;
+ * endif
+ * :initialize task dialog;
+ * :load task data to GUI;
+ * |User|
+ * :input (modify) task data;
+ * if(click button) then (cancel)
+ *   |User|
+ *   end
+ * else (ok)
+ *   |Application|
+ *   :save task instance;
+ *   fork
+ *     |Application|
+ *     :close dialog;
+ *     :refresh GUI;
+ *   fork again
+ *     |Google Drive|
+ *     :synchronize tasks to cloud;
+ *     detach
+ *   |Application|
+ *   endfork
+ *   stop
+ * endif
+ * |Google Drive|
  * @enduml
  */
 @ExperimentalSerializationApi
