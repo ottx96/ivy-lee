e1c454f8c733ccd6e22e0b86b2152fdc5dbe7728
Ott <49874532+ottx96@users.noreply.github.com>
2020-10-11 17:01:38 +0200

    Change Task layout to list (#84)
    * Copied IvyLee.fxml
    
    * Change layout (bootup)
    
    * implement task list
    
    * implement task list
    
    * Implemented list
    
    * created scroll pane
    
    * Created tool bar
    
    * change style of list
    
    * change sizes dynamically
    
    * Refactoring
    
    * Implemented "real" task deletion
    
    * increased scroll speed
    
    * Refactoring
    
    * Refactoring
    
    * Added button icons
    
    * Layout changes
    
    * Refactoring
    
    * changed toolbar layout
    
    * changed scroll pane design
    
    * change default window size
    
    Co-authored-by: Manuel Ott <manuel.ott@x.de>

---
+++ src/main/kotlin/de/ott/ivy/ui/overview/IvyLee.kt
@@ -0,0 +1,150 @@
+package de.ott.ivy.ui.overview
+
+import de.ott.ivy.config.Configuration
+import de.ott.ivy.data.IvyLeeTask
+import de.ott.ivy.data.TaskCellContainer
+import de.ott.ivy.gdrive.ConnectionProvider
+import de.ott.ivy.gdrive.RemoteFilesHandler
+import de.ott.ivy.ui.overview.event.IvyLeeEventHandler
+import de.ott.ivy.ui.overview.impl.ComponentBuilder
+import javafx.event.EventHandler
+import javafx.scene.control.ScrollPane
+import javafx.scene.image.ImageView
+import javafx.scene.layout.*
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.builtins.ListSerializer
+import kotlinx.serialization.cbor.Cbor
+import tornadofx.View
+import tornadofx.minus
+import java.io.File
+import java.lang.Thread.sleep
+import kotlin.math.max
+import kotlin.math.min
+
+
+/**
+ * Main program (UI)
+ * Project: ivy-lee
+ * Package:
+ * Created: 28.01.2020 15:55
+ * @author = manuel.ott
+ * @since = 28. Januar 2020
+ */
+@ExperimentalSerializationApi
+class IvyLee : View("Ivy-Lee Tracking") {
+    override val root: ScrollPane by fxml("/views/IvyLee.fxml")
+
+    companion object {
+        const val MAIN_THREAD_NAME = "UI_THREAD"
+
+        val tasks = mapOf<TaskCellContainer, IvyLeeTask>().toMutableMap()
+        val gdrive = RemoteFilesHandler(ConnectionProvider.connect())
+
+        fun Map<TaskCellContainer, IvyLeeTask>.getCellByBorderPane(bp: BorderPane?) = keys.first { it.borderPane == bp!! }
+        fun Map<TaskCellContainer, IvyLeeTask>.getTaskByBorderPane(bp: BorderPane?) = tasks[keys.first { it.borderPane == bp!! }]
+    }
+
+    val anchorPane: AnchorPane by fxid("anchor_pane")
+    val taskList: VBox by fxid("tasklist")
+    val toolBar: HBox by fxid("tool_bar")
+    val addButton: ImageView by fxid("add")
+    val addButtonPane: Pane by fxid("pane_add")
+    val settingsButton: ImageView by fxid("settings")
+
+    val eventHandler = IvyLeeEventHandler(taskList)
+
+    init {
+        // set scroll speed
+        val SPEED = 0.00175
+        root.content.onScroll = EventHandler {
+            root.vvalue -= it.deltaY * SPEED
+        }
+
+        addButtonPane.prefWidthProperty().bind( root.widthProperty() )
+        addButton.xProperty().bind(addButtonPane.widthProperty().minus( addButton.fitWidthProperty() ).minus(15))
+
+        addButton.setOnMouseClicked {
+            ComponentBuilder.createTaskContainer(IvyLeeTask(), taskList)
+
+            tasks.forEach { it.key.borderPane.apply {
+                minHeightProperty().unbind()
+                minWidthProperty().unbind()
+                minWidthProperty().bind(root.widthProperty().minus(15))
+                minHeightProperty().bind(root.heightProperty().minus(toolBar.heightProperty())
+                        .divide(min(4, max(1, tasks.size)))          //  if less than 4 tasks, scale - scale for 4 tasks max, then scroll for other tasks
+                        .minus(1.35))                                //  border with is 2 px
+
+                onMouseEntered = EventHandler { eventHandler.mark(it) }
+                onMouseExited = EventHandler { eventHandler.unmark(it) }
+                onMousePressed = EventHandler { eventHandler.onClick(it) }
+            }}
+
+            tasks.forEach(eventHandler::updateCell)
+        }
+
+        Thread.currentThread().name = MAIN_THREAD_NAME
+        var oldTasks: List<IvyLeeTask>? = null
+        try {
+            val tasksFile = File("tasks.db")
+            println("downloading tasks from gdrive")
+            gdrive.readTasks(tasksFile)
+            println("loading tasks from file ${tasksFile.absolutePath ?: "NONE"}")
+            oldTasks = Cbor.decodeFromByteArray(ListSerializer(IvyLeeTask.serializer()), tasksFile.readBytes())
+            oldTasks.forEach(::println)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        } catch (t: Throwable) {
+            println("No data stored..")
+        }
+
+        taskList.children.removeAll { it is BorderPane }
+        oldTasks?.ifEmpty { listOf(IvyLeeTask(), IvyLeeTask(), IvyLeeTask()) }?.forEach { task ->
+            val bp = ComponentBuilder.createTaskContainer(task, taskList)
+
+            bp.minWidthProperty().bind(root.widthProperty().minus(15))
+            bp.minHeightProperty().bind(root.heightProperty().minus(toolBar.heightProperty())
+                    .divide(min(4, max(1, oldTasks?.size?:1)))          //  if less than 4 tasks, scale - scale for 4 tasks max, then scroll for other tasks
+                    .minus(1.35))                                          //  border with is 2 px
+
+            bp.onMouseEntered = EventHandler { eventHandler.mark(it) }
+            bp.onMouseExited = EventHandler { eventHandler.unmark(it) }
+            bp.onMousePressed = EventHandler { eventHandler.onClick(it) }
+
+            tasks.forEach(::println)
+            tasks.forEach(eventHandler::updateCell)
+        }
+        when {
+            tasks.isEmpty() -> root.prefHeight = 200.0
+            tasks.size > 4  -> root.prefHeight = 800.0
+            else            -> root.prefHeight = tasks.size * 200.0
+        }
+    }
+
+    init {
+        // auto-sync tasks to gdrive
+        Thread {
+            while (true) {
+                var ct = 0
+                try {
+                    sleep(1000 * 60 * 15) // 15 Minuten
+                    if (++ct % 10 == 0) // 150 Minuten
+                        gdrive.cleanupFilesOlderThan(Configuration.instance.cleanInterval, Configuration.instance.timeUnit)
+                    // write file
+                    println("syncing tasks to gdrive..")
+                    val tasks = File("tasks-auto-sync.db")
+                    tasks.writeBytes(Cbor.encodeToByteArray(ListSerializer(IvyLeeTask.serializer()), Companion.tasks.values.toList()))
+                    gdrive.saveTasks(tasks)
+                } catch (e: java.lang.Exception) {
+                    e.printStackTrace()
+                }
+            }
+        }.apply {
+            isDaemon = true
+            start()
+        }
+    }
+
+
+
+
+}
---
+++ src/main/kotlin/de/ott/ivy/ui/overview/event/IvyLeeEventHandler.kt
@@ -0,0 +1,105 @@
+package de.ott.ivy.ui.overview.event
+
+import de.ott.ivy.data.IvyLeeTask
+import de.ott.ivy.data.TaskCellContainer
+import de.ott.ivy.data.enum.TaskStatus
+import de.ott.ivy.html.MarkdownParser
+import de.ott.ivy.ui.overview.IvyLee
+import de.ott.ivy.ui.overview.IvyLee.Companion.getCellByBorderPane
+import de.ott.ivy.ui.overview.IvyLee.Companion.getTaskByBorderPane
+import de.ott.ivy.ui.dialog.TaskDialog
+import javafx.scene.input.MouseEvent
+import javafx.scene.layout.BorderPane
+import javafx.scene.layout.VBox
+import javafx.scene.paint.Color
+import tornadofx.CssBox
+import tornadofx.style
+
+class IvyLeeEventHandler(private val taskList: VBox) {
+
+    fun mark(event: MouseEvent) {
+        (event.target as BorderPane).style {
+            borderColor += CssBox(Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE)
+            backgroundColor += IvyLee.tasks.getTaskByBorderPane(event.target as BorderPane)!!.status.color.desaturate()
+        }
+    }
+
+    fun unmark(event: MouseEvent) {
+        (event.target as BorderPane).style {
+            backgroundColor += IvyLee.tasks.getTaskByBorderPane(event.target as BorderPane)!!.status.color
+            borderColor += CssBox(Color.BLACK, Color.BLACK, Color.BLACK, Color.BLACK)
+        }
+    }
+
+    fun onClick(event: MouseEvent) {
+        if (event.isPrimaryButtonDown)
+            (event.target as BorderPane).apply {
+                val task = IvyLee.tasks.getTaskByBorderPane(this)!!
+                when (task.status) {
+                    TaskStatus.EMPTY -> {
+                        return
+                    }
+                    TaskStatus.UNDONE -> task.status = TaskStatus.IN_WORK
+                    TaskStatus.IN_WORK -> task.status = TaskStatus.DONE
+                    TaskStatus.DONE -> task.status = TaskStatus.UNDONE
+                }
+                updateCell(IvyLee.tasks.getCellByBorderPane(this), task)
+                if (task.status == TaskStatus.IN_WORK) {
+                    // stop other timers
+                    IvyLee.tasks.values.filter { it.status == TaskStatus.IN_WORK && it != task }.forEach {
+                        it.status = TaskStatus.UNDONE
+                    }
+                    Thread {
+                        val threadCell = IvyLee.tasks.getCellByBorderPane(this)
+                        while (task.status == TaskStatus.IN_WORK) {
+                            task.timeInvestedSeconds++
+                            updateCell(threadCell, task)
+                            Thread.sleep(1000) // sleep 1s
+                        }
+                    }.start()
+                }
+            }
+        else {
+            // open custom dialog
+            (event.target as BorderPane).apply {
+                val newTask = TaskDialog.showDialog(IvyLee.tasks.getTaskByBorderPane(this)!!)
+                if (newTask.status == TaskStatus.EMPTY) {
+                    val cell = IvyLee.tasks.getCellByBorderPane(this)
+                    taskList.children.remove(cell.borderPane)
+                    IvyLee.tasks.remove(cell)
+                } else {
+                    IvyLee.tasks[IvyLee.tasks.getCellByBorderPane(this)] = newTask
+                    updateCell(IvyLee.tasks.getCellByBorderPane(this), IvyLee.tasks.getTaskByBorderPane(this)!!)
+                    mark(event)
+                }
+            }
+        }
+    }
+
+    fun updateCell(cellContainer: TaskCellContainer, task: IvyLeeTask) {
+        println("updating cell with task: $task")
+
+        cellContainer.titleLabel.text = task.name
+        if (cellContainer.descLabel.engine.userStyleSheetLocation.isNullOrBlank())
+            cellContainer.descLabel.engine.userStyleSheetLocation = javaClass.getResource("/de/ott/ivy/css/style.css").toString()
+        cellContainer.timeLabel.text = "${task.estTimeSeconds / 60.0} m"
+
+        cellContainer.progressBar.progress = task.timeInvestedSeconds * 1.0 / task.estTimeSeconds
+        cellContainer.statusIndicator.progress = task.timeInvestedSeconds * 1.0 / task.estTimeSeconds
+        if (task.status == TaskStatus.DONE) cellContainer.statusIndicator.progress = 1.0
+
+        if (task.timeInvestedSeconds >= task.estTimeSeconds)
+            cellContainer.progressBarAdditional.progress = (task.timeInvestedSeconds - task.estTimeSeconds) * 1.0 / task.estTimeSeconds
+        else
+            cellContainer.progressBarAdditional.progress = 0.0
+
+        cellContainer.borderPane.style {
+            backgroundColor += task.status.color
+            borderColor += CssBox(Color.BLACK, Color.BLACK, Color.BLACK, Color.BLACK)
+        }
+
+        if (Thread.currentThread().name == IvyLee.MAIN_THREAD_NAME)
+            cellContainer.descLabel.engine.loadContent(MarkdownParser.convertHtml(task.descr), "text/html")
+    }
+
+}
